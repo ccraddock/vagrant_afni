7ed0492bccfb4e8422f05eb65021a7e5dc3f47b4
Cameron Craddock <cameron.craddock@gmail.com>
2016-03-29 15:20:03 -0300

    updates

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install apt-file
+apt-file update
+apt-get install -y libxext-dev 
+apt-get install -y libxmu-headers 
+apt-get install -y libmotif-dev 
+apt-get install -y libgiftiio-dev 
+apt-get install -y libexpat1-dev 
+apt-get install -y libxft-dev
+apt-get install -y libxmu-dev 
+apt-get install -y libxpm-dev 
+apt-get install -y libgslk0-dev 
+apt-get install -y mesa-common-dev 
+apt-get install -y libglu1-mesa-dev 
+apt-get install -y libxi-dev
+apt-get install -y libglib2.0-dev 
+apt-get install -y libxt-dev 
+apt-get install -y m4
+apt-get install -y libxp-dev
+apt-get install -y libgsl10-dev
+apt-get install -y htop
