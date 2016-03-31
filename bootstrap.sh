5110fcd579e7e5fddbb6bb4f43aed77fc0da0a37
Cameron Craddock <cameron.craddock@gmail.com>
2016-03-31 13:36:31 -0300

    fixed install script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install apt-file
+apt-get install -y apt-file
 apt-file update
 apt-get install -y libxext-dev 
 apt-get install -y libxmu-headers 
@@ -18,5 +18,12 @@ apt-get install -y libglib2.0-dev
 apt-get install -y libxt-dev 
 apt-get install -y m4
 apt-get install -y libxp-dev
-apt-get install -y libgsl10-dev
+apt-get install -y libgsl0-dev
 apt-get install -y htop
+apt-get install -y git
+apt-get install -y tcsh
+apt-get install -y electric-fence
+apt-get install -y gdb
+apt-get install -y valgrind
+
+echo "export PATH=/vagrant/afni/src/linux_openmp_64:$PATH" >> /home/vagrant/.bashrc
